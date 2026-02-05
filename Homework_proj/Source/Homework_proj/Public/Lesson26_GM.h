22b96e1b3cf51bdae17536b7384936a1b0c05df5
Moirslow <slowmorsy@gmail.com>
2026-02-05 16:43:23 +0100

    Lesson26

---
+++ Homework_proj/Source/Homework_proj/Public/Lesson26_GM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Lesson26_GM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOMEWORK_PROJ_API ALesson26_GM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
